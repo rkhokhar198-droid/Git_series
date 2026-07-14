4cb140750b434c5a8a344beb22f930df8f794b2f
rajakhan <rkhokhar198@gmail.com>
2026-07-14 11:42:07 +0100

    second commit with updated file

--- first_file.py
+++ first_file.py
@@ -1,1 +1,7 @@
  # This is new file and first commit
+
+
+# This is the code for the first file in the repository. It contains some basic functionality and serves as a starting point for the project.
+
+
+print("Hello, this is the first file in the repository!")
